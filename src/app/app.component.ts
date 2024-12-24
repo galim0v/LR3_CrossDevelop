83f9677319c7bb494a763f66e3528b31b289cfc4
galim0v <flan2010@mail.ru>
2024-12-24 22:00:53 +0300

    1 пункт и частичка 2
    добавить базовые элементы страницы: header (с векторным логотипом), footer, menu;
    добавить форму авторизации с валидацией; (пока никуда не летит, просто существует форма)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { routes } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/auth/auth.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent {
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.authForm.valid) {
+      console.log(this.authForm.value);
+    }
+  }
+}
